fcaf4de0b91bafc728a61f73e59ec2c3e1d54f8d
Hailucsp <hailuc@csp.edu>
2024-12-01 16:46:53 -0600

    Update Character.java

--- Simulation/src/main/java/Character.java
+++ Simulation/src/main/java/Character.java
@@ -8,16 +8,18 @@
  * @author chris, connor
  */
 public class Character {
+    // variables
     protected int health;
     protected int attack;
+    // boolean variable to indicate if the character is alive or not
     public boolean alive;
-
+    // Character constructor with health and attack parameters
     public Character(int health, int attack) {
         this.health = health;
         this.attack = attack;
         this.alive = true;
     }
-
+// getters and setters for the variables
     public int getHealth() {
         return health;
     }
@@ -41,14 +43,17 @@ public class Character {
     public void setAlive(boolean alive) {
         this.alive = alive;
     }
-
+    // method attacked 
     public void attacked(int damage) {
+        // health = health - damage
         this.health -= damage;
+        //if health is less than or equal to 0
         if (this.health <= 0) {
+            // sets alive boolean value to false meaning the character is dead
             this.alive = false;
         }
     }
-
+    
     public void attacking(Character target) {
         if (this.alive) {
             target.attacked(this.attack);
