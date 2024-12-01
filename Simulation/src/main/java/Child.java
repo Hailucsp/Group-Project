d0938a554f4029ac3243bb38068506ca5d9d1a12
Hailucsp <hailuc@csp.edu>
2024-12-01 16:30:12 -0600

    Update Child.java

--- Simulation/src/main/java/Child.java
+++ Simulation/src/main/java/Child.java
@@ -8,7 +8,10 @@
  * @author chris, connor
  */
 public class Child extends Character {
+    // Child constructor 
     public Child() {
+        // super that extends to the Character class's consturctor attributes.
+        // first number being the health, second number being the attack
         super(20, 2);
     }
 }
